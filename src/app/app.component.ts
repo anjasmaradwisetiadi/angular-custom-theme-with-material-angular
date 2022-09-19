fe3bdecc94dfd7aeac850a9193a045804683e760
anjasmaradwisetiadi <anjasmara.setiadi@zettabyte.life>
2022-09-20 02:58:34 +0700

    successfull make theme custom when use ngClass based current template choice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
-  currentTheme = 'dark_theme'
+  currentTheme = 'pink-theme'
   constructor() {
   }
 
