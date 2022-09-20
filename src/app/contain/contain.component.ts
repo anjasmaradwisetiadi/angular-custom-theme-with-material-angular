4a59aac75490515d0441af39ac255ea42ac0dca2
anjasmaradwisetiadi <anjasmara.setiadi@zettabyte.life>
2022-09-20 15:02:33 +0700

    add validation form error and upadate theme style

--- src/app/contain/contain.component.ts
+++ src/app/contain/contain.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-contain',
   templateUrl: './contain.component.html',
   styleUrls: ['./contain.component.scss']
 })
+
 export class ContainComponent implements OnInit {
 
-  constructor() { }
+  mainFormAll: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+  ) { }
+  
 
   ngOnInit(): void {
+    this.initMainForm()
   }
 
+  initMainForm(){
+    this.mainFormAll = this.fb.group({
+      emailFormControl: [null, Validators.required]
+    })
+  }
 }
