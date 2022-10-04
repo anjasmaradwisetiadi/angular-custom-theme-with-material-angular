cadebf7864348445afbc9d7511cb5375e964254e
anjasmaradwisetiadi <anjasmara.setiadi@zettabyte.life>
2022-10-04 15:22:23 +0700

    reduce trash code . fixing theming color pallete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
+  title = 'custom-theming-angular';
+
   currentTheme = 'murasaki-theme'
   constructor() {
   }
 
   ngOnInit() {
-    console.log('currentTheme = ', this.currentTheme)
+
   }
-  title = 'custom-theming-angular';
 
   acceptTheme(params: string){
     this.currentTheme = params;
-    console.log('params')
-    console.log(params)
   }
 }
